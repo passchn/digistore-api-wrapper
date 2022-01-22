7432445fc137d38c6587bef0e6008a9bc1fa28bc
passchn <77938819+passchn@users.noreply.github.com>
2022-01-23 00:08:36 +0100

    check connection, save exceptions

--- src/DigistoreClient.php
+++ src/DigistoreClient.php
@@ -3,10 +3,13 @@
 namespace DigistoreApi;
 
 use DigistoreApi\Digistore\DigistoreApi;
+use DigistoreApi\Digistore\DigistoreApiException;
+use Nette\Utils\Arrays;
 
 class DigistoreClient
 {
     private DigistoreApi $api;
+    private array $errors = [];
 
     public function __construct(string $api_key)
     {
@@ -15,6 +18,29 @@ class DigistoreClient
 
     public function isConnected()
     {
-        $this->api->ping();
+        try {
+            $this->api->ping();
+        } catch (DigistoreApiException $e) {
+            $this->errors[] = $e;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function getLastError(): ?string
+    {
+        /**
+         * @var \Exception|null $exception
+         */
+        $exception = Arrays::last($this->errors);
+
+        return $exception ? $exception->getMessage() : null;
     }
 }
